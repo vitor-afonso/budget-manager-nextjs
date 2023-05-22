4f198604beaf67743a21452682bea233ba18651c
Vitor Afonso <vittokm@hotmail.com>
2023-05-22 17:40:05 +0100

    Add link to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
       <h1>Landing Page</h1>
+      <Link href={'/login'}>Go to Login</Link>
     </main>
   );
 }
