f7e7249dc3de8e041618afc4bb2cee92f538a50e
Vitor Afonso <vittokm@hotmail.com>
2023-05-22 17:39:25 +0100

    Add context to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/src/app/globals.css';
 import { Inter } from 'next/font/google';
+import { AuthProviderWrapper } from '@/src/app/auth.context';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthProviderWrapper>{children}</AuthProviderWrapper>
+      </body>
     </html>
   );
 }
