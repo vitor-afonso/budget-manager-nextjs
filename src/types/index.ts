f268bc713907e3324e985065080e45b14f79358f
Vitor Afonso <vittokm@hotmail.com>
2023-05-22 18:29:04 +0100

    Create Types file

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+export interface IUser {
+  name: string;
+  email: string;
+  _id: string;
+  months?: IMonth[];
+}
+export interface ILoginUser {
+  email: string;
+  password: string;
+}
+export interface ISignupUser extends ILoginUser {
+  name: string;
+}
+
+export interface IIncome {
+  _id: string;
+  category: String;
+  amount: number;
+  monthId: string;
+  createdAt: Date;
+  updateddAt: Date;
+}
+export interface IExpense {
+  _id: string;
+  title: String;
+  category: String;
+  amount: number;
+  monthId: string;
+  createdAt: Date;
+  updateddAt: Date;
+}
+export interface IMonth {
+  _id: string;
+  incomes: IIncome[];
+  expenses: IExpense[];
+  userId: string;
+  createdAt: Date;
+  updateddAt: Date;
+  deleted: boolean;
+}
