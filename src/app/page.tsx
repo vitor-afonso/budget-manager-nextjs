a0500466f2d8c6e3258d222dcfc20c7c406519dc
Vitor Afonso <vittokm@hotmail.com>
2023-05-23 20:40:18 +0100

    Create Month component and add to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link';
 
+import Month from '@/src/app/components/Month';
+
 export default function Home() {
   return (
     <main>
       <h1>Landing Page</h1>
+      {/* Add component that will hold the Month and the CreateMonth */}
+      <Month />
       <Link href={'/login'}>Go to Login</Link>
     </main>
   );
