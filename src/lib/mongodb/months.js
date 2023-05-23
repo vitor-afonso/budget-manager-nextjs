0dd647d1f0dd95417e2e9c950146d140d7f6076c
Vitor Afonso <vittokm@hotmail.com>
2023-05-23 20:33:46 +0100

    install mongodb and setup connection to db

---
+++ src/lib/mongodb/months.js
@@ -0,0 +1,60 @@
+import clientPromise from '.';
+
+let client;
+let db;
+let months;
+
+async function init() {
+  if (db) return;
+  try {
+    client = await clientPromise;
+    db = await client.db();
+    months = db.collection('months');
+  } catch (error) {
+    throw new Error('Failed to stablish connection to database.');
+  }
+}
+
+(async () => {
+  await init();
+})();
+
+export async function getMonths() {
+  try {
+    if (!months) await init();
+    const result = await months.find({ deleted: false }).toArray();
+
+    // Fetch incomes for each month
+    const monthIds = result.map((month) => month._id);
+    const incomes = await db
+      .collection('incomes')
+      .find()
+      .map((income) => ({ ...income, _id: income._id.toString(), monthId: income.monthId.toString() }))
+      .toArray();
+
+    // Fetch expenses for each month
+    const expenses = await db
+      .collection('expenses')
+      .find()
+      .map((expense) => ({ ...expense, _id: expense._id.toString(), monthId: expense.monthId.toString() }))
+      .toArray();
+
+    // Merge incomes and expenses with their respective months
+    const populatedMonths = result.map((month) => {
+      const monthId = month._id.toString();
+      const userId = month.userId.toString();
+      const monthIncomes = incomes.filter((income) => income.monthId.toString() === monthId);
+      const monthExpenses = expenses.filter((expense) => expense.monthId.toString() === monthId);
+      return {
+        ...month,
+        _id: monthId,
+        incomes: monthIncomes,
+        expenses: monthExpenses,
+        userId,
+      };
+    });
+    return { months: populatedMonths };
+  } catch (error) {
+    return { error: 'Failed to fetch months!' };
+  }
+}
