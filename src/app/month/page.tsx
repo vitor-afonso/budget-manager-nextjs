58fdab5a7691b646acaeac8fe3002905ba6206aa
Vitor Afonso <vittokm@hotmail.com>
2023-06-02 22:40:28 +0100

    Create month page to see month details graph

---
+++ src/app/month/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+import MonthCategoryGraph from '@/components/MonthCategoryGraph';
+import { IMonth } from '@/types/models';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext, IAppContext } from '../auth.context';
+import { isSameMonth } from 'date-fns';
+import { APP } from '@/utils/app.constants';
+import MonthYearHeader from '@/components/MonthYearHeader';
+
+const MonthInfo = () => {
+  const { userMonths } = useContext(AuthContext) as IAppContext;
+  const [currentMonth, setCurrentMonth] = useState<IMonth | null>(null);
+  const [monthIndex, setMonthIndex] = useState<number | null>(null);
+
+  // set current month
+  useEffect(() => {
+    if (userMonths.length > 0) {
+      let month = userMonths.find((oneMonth, i) => {
+        if (isSameMonth(new Date(), oneMonth.createdAt)) {
+          setMonthIndex(i);
+          return oneMonth;
+        }
+      });
+      if (month) {
+        setCurrentMonth(month);
+      }
+    }
+  }, [userMonths]);
+
+  return (
+    <div className='flex flex-col items-center'>
+      {currentMonth && (
+        <>
+          <MonthYearHeader index={monthIndex} currentMonthYear={currentMonth} setCurrentMonthYear={setCurrentMonth} setIndex={setMonthIndex} />
+          <MonthCategoryGraph incomeExpenseList={currentMonth.incomes} categoryType={APP.eventType.income} />
+          <MonthCategoryGraph incomeExpenseList={currentMonth.expenses} categoryType={APP.eventType.expense} />
+        </>
+      )}
+      {!currentMonth && <p>No month available to be displayed</p>}
+    </div>
+  );
+};
+
+export default MonthInfo;
