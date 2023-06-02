c149ee376797a9e69cf2ad7584b8dee16aa027eb
Vitor Afonso <vittokm@hotmail.com>
2023-06-02 22:35:42 +0100

    install chartjs and create category graph

---
+++ src/components/MonthCategoryGraph.tsx
@@ -0,0 +1,43 @@
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ChartOptions, ArcElement, Tooltip, Legend } from 'chart.js';
+import { IExpense, IIncome } from '@/types/models';
+import { getCategoryNamestoShow, getCategoryTotals, getGraphColors } from '@/utils/app.methods';
+import { capitalize } from 'lodash';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const MonthCategoriesGraph = ({ incomeExpenseList, categoryType }: { incomeExpenseList: IIncome[] | IExpense[]; categoryType: string }): JSX.Element => {
+  const { categoryTotals } = getCategoryTotals(incomeExpenseList);
+  const data = {
+    labels: getCategoryNamestoShow(incomeExpenseList, categoryTotals),
+    datasets: [
+      {
+        label: 'Total €',
+        data: Object.values(categoryTotals),
+        backgroundColor: getGraphColors(categoryType),
+        borderColor: '#000',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options: ChartOptions = {
+    //events: ['click'],
+    plugins: {
+      legend: {
+        display: true,
+        labels: { color: '#f3f4f6' },
+        position: 'top',
+      },
+    },
+  };
+  return (
+    <div className='flex flex-col items-center text-gray-100 mb-4'>
+      <h1 className='text-xl font-semibold my-4'>{capitalize(categoryType)}s by category</h1>
+      {incomeExpenseList.length > 0 && <Pie data={data} options={options} />}
+      {incomeExpenseList.length === 0 && <p className='text-gray-400'>{`No ${categoryType} to display.`}</p>}
+    </div>
+  );
+};
+
+export default MonthCategoriesGraph;
