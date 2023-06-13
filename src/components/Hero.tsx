9561da4e4872b1517018c8d45514bd6ec88c27a3
Vitor Afonso <vittokm@hotmail.com>
2023-06-13 11:07:51 +0100

    Create structure for modal component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { isSameMonth } from 'date-fns';
 import { AuthContext, IAppContext } from '@/app/auth.context';
+import ModalCreateMonth from './ModalCreateMonth';
 
 const Hero = () => {
   const { user, userMonths } = useContext(AuthContext) as IAppContext;
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const isCurrentMonthOpen = () => {
     if (userMonths.length > 0) {
@@ -18,7 +20,12 @@ const Hero = () => {
   return (
     <section className='w-full md:w-80'>
       {!user && <div className='w-full md:w-80 h-80 bg-slate-400 mb-4'></div>}
-      {!isCurrentMonthOpen() && <button className='border border-black text-xl rounded-3xl h-10 bg-slate-200 w-full hover:text-gray-200 hover:bg-slate-400 duration-300'>Open new Month</button>}
+      {!isCurrentMonthOpen() && !isModalOpen && (
+        <button className='border border-black text-xl rounded-3xl h-10 bg-slate-200 w-full hover:text-gray-200 hover:bg-slate-400 duration-300' onClick={() => setIsModalOpen(true)}>
+          Open new Month
+        </button>
+      )}
+      {isModalOpen && <ModalCreateMonth isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />}
     </section>
   );
 };
---
+++ src/components/ModalCreateMonth.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+/* interface Props {
+  isModalOpen: boolean;
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  mainFunction(): void;
+  question: string;
+  buttonText: string;
+} */
+
+/* const Modal = ({ isModalOpen, setIsModalOpen, mainFunction, question, buttonText }: Props) => {
+  return (
+    <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-xs bg-slate-400 border-2 border-purple-800 shadow-24 p-4 flex flex-col items-center'>
+    <p className='text-gray-200'>Custom Modal</p>
+    </div>
+    );
+  }; */
+
+interface Props {
+  isModalOpen: boolean;
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const ModalCreateMonth = ({ isModalOpen, setIsModalOpen }: Props) => {
+  const handleCreateMonth = () => {
+    setIsModalOpen(false);
+    // call createMonth(some data)
+    // upddate userMonths
+  };
+  return (
+    <div className='absolute top-0 left-0 z-10 w-screen h-screen '>
+      <div className='absolute z-30 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-xs bg-slate-500 border-2 border-slate-800 rounded-3xl shadow-24 p-4 flex flex-col items-center'>
+        <p className='text-gray-200'>Custom Modal</p>
+        {/* CREATE FORM */}
+      </div>
+      <div className='bg-black/50 w-full h-full absolute z-20'></div>
+    </div>
+  );
+};
+
+export default ModalCreateMonth;
