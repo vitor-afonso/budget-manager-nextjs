3818beaa26c814f9f228ea811cb9a280139fe1d7
Vitor Afonso <vittokm@hotmail.com>
2023-06-25 22:55:25 +0100

    Update form and submit handler

--- src/components/ModalCreateIncomeExpense.tsx
+++ src/components/ModalCreateIncomeExpense.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { APP } from '@/utils/app.constants';
+import { createIncomeExpense } from '@/services/incomesExpenses';
+import { AuthContext, IAppContext } from '@/app/auth.context';
 
 interface Props {
   isModalOpen: boolean;
@@ -11,41 +13,56 @@ interface Props {
   eventType: string;
 }
 const schema = z.object({
+  title: z.string().optional(),
   category: z.string(),
   amount: z.string().transform((val) => Number(val)),
+  creationDate: z.date(),
 });
 //get type from schema
 type FormData = z.infer<typeof schema>;
 
 const ModalCreateIncomeExpense = ({ isModalOpen, setIsModalOpen, monthId, eventType }: Props) => {
+  const { updateMonthIncomeExpenseCreation } = useContext(AuthContext) as IAppContext;
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>();
+  const isExpense = eventType === APP.eventType.expense;
 
-  const handleCreateMonth = ({ category, amount }: FormData) => {
+  const handleCreateMonth = async (formData: FormData) => {
     if (!monthId) return;
 
-    const reqBody = {
-      category,
-      amount,
-      monthId,
-    };
+    try {
+      const requestBody = {
+        ...formData,
+        category: formData.category.toLowerCase(),
+        monthId,
+      };
 
-    console.log(reqBody);
-
-    // call createMonth(reqBody)
-    // upddate month Income/Expense
-    // setIsModalOpen(false);
+      const createdIncomeExpense = await createIncomeExpense(requestBody, isExpense);
+      updateMonthIncomeExpenseCreation(createdIncomeExpense, monthId);
+    } catch (error) {
+      console.log(error);
+      // setErrorMessage(error)
+    } finally {
+      setIsModalOpen(false);
+    }
   };
 
   return (
     <div className='absolute top-0 left-0 z-10 w-screen h-screen '>
       <div className='absolute z-30 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-xs bg-slate-500 border-2 border-slate-800 rounded-3xl shadow-24 p-4 flex flex-col items-center'>
-        <p className='text-gray-200'>{eventType === APP.eventType.income ? 'Add income' : 'Add expense'}</p>
+        <p className='text-gray-200'>{isExpense ? 'Add expense' : 'Add income'}</p>
 
         <form onSubmit={handleSubmit(handleCreateMonth)}>
+          {isExpense && (
+            <label>
+              <p>Title</p>
+              <input {...register('title', { required: 'Title is required' })} />
+            </label>
+          )}
+          {errors.title?.message && <p className='text-red-500 font-bold text-sm'>{errors.title.message}</p>}
           <label>
             <p>Category</p>
             <input {...register('category', { required: 'Category is required' })} />
@@ -62,6 +79,17 @@ const ModalCreateIncomeExpense = ({ isModalOpen, setIsModalOpen, monthId, eventT
             />
           </label>
           {errors.amount?.message && <p className='text-red-500 font-bold text-sm'>{errors.amount.message}</p>}
+
+          <label>
+            <p>Date</p>
+            <input
+              type='date'
+              {...register('creationDate', {
+                required: 'Date is required',
+              })}
+            />
+          </label>
+          {errors.creationDate?.message && <p className='text-red-500 font-bold text-sm'>{errors.creationDate.message}</p>}
           <br />
           <input type='submit' />
         </form>
