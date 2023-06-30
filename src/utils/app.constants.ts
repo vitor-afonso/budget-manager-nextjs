f159b46b9a192e87e15582be2ad89c8470a5dcfb
Vitor Afonso <vittokm@hotmail.com>
2023-06-30 21:41:29 +0100

    update projectApi url

--- src/utils/app.constants.ts
+++ src/utils/app.constants.ts
@@ -24,5 +24,5 @@ export const APP = {
   monthsOfTheYear: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
   graphColdColors: ['#7FA2C0', '#419CAA', '#057692', '#004D6A', '#002842', '#00131F', '#000C16', '#00060D', '#000105', '#000001'],
   graphWarmColors: ['#FFDFB3', '#FFAD66', '#FF7F1A', '#E86100', '#BF4800', '#973100', '#832600', '#6F1B00', '#5B0F00', '#470400', '#330000'],
-  projectApi: `http://localhost:5005/api`,
+  projectApi: `https://budget-manager-server.onrender.com/api`,
 };
