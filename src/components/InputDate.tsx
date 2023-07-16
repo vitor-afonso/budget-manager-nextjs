635027ae6e622a86d3d68e09827aa7979de4a332
Vitor Afonso <vittokm@hotmail.com>
2023-07-16 17:21:44 +0100

    Add div to fix width not being applyed on safari

--- src/components/InputDate.tsx
+++ src/components/InputDate.tsx
@@ -14,15 +14,18 @@ const InputDate = ({ register, errors, inputName, monthDate }: Props) => {
     <div>
       <label className='text-gray-300 text-lg capitalize'>
         Date
-        <input
-          type={APP.inputName.date}
-          {...register(inputName, {
-            required: 'Date is required',
-          })}
-          className='mt-1 w-full h-12 rounded-md px-2 border border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-400'
-          min={getMinMaxDate(monthDate, 'min')}
-          max={getMinMaxDate(monthDate, 'max')}
-        />
+        {/* div to fix safari not applying w-full to input */}
+        <div className='w-full'>
+          <input
+            type={APP.inputName.date}
+            {...register(inputName, {
+              required: 'Date is required',
+            })}
+            className='w-full h-12 rounded-md px-2 border border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-400'
+            min={getMinMaxDate(monthDate, 'min')}
+            max={getMinMaxDate(monthDate, 'max')}
+          />
+        </div>
       </label>
       {errors[`${inputName}`]?.message && <p className='text-red-500 font-bold text-sm'>{errors[`${inputName}`].message}</p>}
     </div>
