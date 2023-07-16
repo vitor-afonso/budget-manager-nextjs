f8d014b69916f0a6c457bec7fc83a1d0965e519d
Vitor Afonso <vittokm@hotmail.com>
2023-07-16 16:01:41 +0100

    Update input props and style

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -5,17 +5,18 @@ interface Props {
   errors: any;
   inputName: string;
   inputType?: string;
+  inputRules: any;
 }
 
-const InputText = ({ register, errors, inputName, inputType }: Props) => {
+const InputText = ({ register, errors, inputName, inputType, inputRules }: Props) => {
   return (
     <div>
       <label className='text-gray-300 text-lg capitalize'>
         {inputName}
         <input
           type={inputType}
-          {...register(inputName, { required: `${inputName} is required` })}
-          className='mt-1 w-full h-8 rounded-md px-2 border border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-400'
+          {...register(inputName, inputRules)}
+          className='mt-1 w-full h-12 text-gray-800 rounded-md px-2 border border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-400'
         />
       </label>
       {errors[`${inputName}`]?.message && <p className='text-red-500 font-bold text-sm'>{errors[`${inputName}`].message}</p>}
