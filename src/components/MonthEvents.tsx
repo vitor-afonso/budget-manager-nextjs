5114dba75d98f9f3607f1f9a16eea86c6c43d81c
Vitor Afonso <vittokm@hotmail.com>
2023-07-16 15:56:03 +0100

    Update font-weight

--- src/components/MonthEvents.tsx
+++ src/components/MonthEvents.tsx
@@ -18,7 +18,7 @@ const MonthEvents = ({ events, eventType, monthId }: Props): JSX.Element => {
     <div className='flex flex-col justify-between pb-4 border border-black max-h-64 rounded-3xl mt-4 bg-slate-200 w-full'>
       <div className='mb-2 w-full'>
         <div className='border border-black text-xl rounded-3xl h-10 w-full flex items-center justify-center bg-slate-100'>
-          <h2>{eventType === APP.eventType.income ? 'Incomes' : 'Expenses'}</h2>
+          <h2 className='font-semibold'>{eventType === APP.eventType.income ? 'Incomes' : 'Expenses'}</h2>
         </div>
         <div className={clsx(screen.height < 800 ? 'max-h-20' : 'max-h-32', 'pb-2 pl-5 mt-5 mr-5 overflow-y-auto')}>
           {events
