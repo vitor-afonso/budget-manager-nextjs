0aae239bd65d047286d5cd87737e6b0d8170f139
Vitor Afonso <vittokm@hotmail.com>
2023-12-17 20:25:17 +0000

    Update component height

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const Spinner = ({ size }:{ size?: number }) => {
   return <div className="text-center">
-    <Image src="./images/budget-spinner.svg" alt="spinner" width={size || 100} height={100} />
+    <Image src="./images/budget-spinner.svg" alt="spinner" width={size || 100} height={size || 100} />
   </div>;
 };
 
