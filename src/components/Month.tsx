91cbc8c5d0398809a5e36844f56e725e0bcc29b8
Vitor Afonso <vittokm@hotmail.com>
2023-12-29 20:12:13 +0000

    Create initial logic to get the week total

--- src/components/Month.tsx
+++ src/components/Month.tsx
@@ -18,6 +18,7 @@ export default function Month(): JSX.Element {
   ) as IUserDataContext;
   const [currentMonth, setCurrentMonth] = useState<IMonth | IYear | null>(null);
   const [monthIndex, setMonthIndex] = useState<number | null>(null);
+  const [spentThisWeek, setSpentThisWeek] = useState<number | null>(null);
   const [monthId, setMonthId] = useState<string>('');
 
   useEffect(() => {
@@ -52,6 +53,66 @@ export default function Month(): JSX.Element {
     if (currentMonth && '_id' in currentMonth) {
       setMonthId(currentMonth._id);
     }
+    const myDate = new Date();
+
+    let dayOfTheWeek = myDate.getUTCDay();
+    let dayOfTheMonth = myDate.getUTCDate();
+    let totalWeekExpenses = 0
+    let daysFromPreviousMonth = 0;
+    
+    // get all week days
+    let allWeekDaysDates: any = [];
+    
+    for (let index = 0; index <= myDate.getUTCDay(); index++) {
+     
+      // set number of days that belongs to previous month
+      if(dayOfTheMonth - index < 0){
+        daysFromPreviousMonth++
+
+      } else {
+        allWeekDaysDates.push(dayOfTheMonth - index)
+      }
+    }
+
+    // calculate expenses total 
+    currentMonth?.expenses.forEach((e, i) => {
+      let expenseDay = new Date(e.createdAt).getUTCDate();
+
+      if(allWeekDaysDates.includes(expenseDay) && e.category.toLowerCase() !== 'bills'){
+        totalWeekExpenses += e.amount
+      }
+    })
+
+    // get previous month and add expenses from last number of days
+
+    const isPrevMonthFromThisYear = new Date().getMonth() !== 0;
+    let yearOfPrevMonth = isPrevMonthFromThisYear ? new Date().getFullYear() : new Date().getFullYear() - 1;
+    let previousMonthNumber = isPrevMonthFromThisYear ? new Date().getMonth() - 1 : 11;
+    // must have +1 because of the 0 representing the last day of the month
+    const lastDayOfPreviousMonth = new Date(yearOfPrevMonth, previousMonthNumber + 1, 0).getDate();
+
+
+    // find previous month
+
+    let previousMonth = userMonths.find(m => new Date(m.createdAt).getFullYear() === yearOfPrevMonth && new Date(m.createdAt).getMonth() === previousMonthNumber);
+
+  
+    // loop through the number of days in the previous month
+
+    for (let index = 0; index < daysFromPreviousMonth; index++){
+
+      let dayExpenses = previousMonth?.expenses.filter(e => new Date(e.createdAt).getUTCDate() === lastDayOfPreviousMonth - index)
+
+      let allExpensesAmount = dayExpenses?.map(e => e.amount)
+
+      let sum = allExpensesAmount?.reduce((total, num) => total + num)
+
+      totalWeekExpenses += sum!
+    }
+
+    setSpentThisWeek(totalWeekExpenses);
+
+
   }, [currentMonth]);
 
   return (
@@ -65,6 +126,7 @@ export default function Month(): JSX.Element {
             setCurrentMonthYear={setCurrentMonth}
             setIndex={setMonthIndex}
           />
+          {spentThisWeek && <div> <span>Spent: </span> <span>{spentThisWeek}</span></div>}
           <MonthEvents
             events={currentMonth.incomes}
             eventType={APP.eventType.income}
