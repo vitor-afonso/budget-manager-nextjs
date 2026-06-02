f45ac3e2baa1e28f85b6c2806760417923822de6
Vitor Afonso <vittokm@hotmail.com>
2026-06-02 21:50:15 +0100

    Update project API URL in app.constants.ts for production deployment

--- src/utils/app.constants.ts
+++ src/utils/app.constants.ts
@@ -104,8 +104,8 @@ export const APP = {
     '#470400',
     '#330000',
   ],
-  // projectApi: 'https://budget-manager-server.onrender.com/api',
-  projectApi: 'http://localhost:5005/api',
+  projectApi: 'https://budget-manager-server.onrender.com/api',
+  //projectApi: 'http://localhost:5005/api',
 };
 
 export function getCurrencyFormatter(locale: string) {
